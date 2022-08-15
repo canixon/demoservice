813df20fd559d82106150a523c061de5da4cc7ef
Chris <christopherdahl.nixon@gmail.com>
2022-08-15 11:33:27 -0400

    First commit

---
+++ src/main/java/com/bah/msd/mcc/Bahprojp2Application.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bahprojp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bahprojp2Application.class, args);
+	}
+	//
+}
