ec0d81b10e96fc912250cbc373c4e3149ef7fd3e
Chris <christopherdahl.nixon@gmail.com>
2022-08-22 10:43:08 -0400

    working

---
+++ bahprojday3/src/main/java/com/bah/msd/mcc/Bahprojday3Application.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bahprojday3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bahprojday3Application.class, args);
+	}
+
+}
